f2ec240b85c6790199083c325d5b78c61a2ce813
frumsy <liambritt3@gmail.com>
2020-10-02 00:25:08 -0400

    Updates

---
+++ precache-manifest.522fc0f1651e9d3322541cbecf40aa9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad1df697d8844048e9187959175c91a",
+    "url": "/poke/index.html"
+  },
+  {
+    "revision": "daf5cb05d565b7529dbb",
+    "url": "/poke/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "daf5cb05d565b7529dbb",
+    "url": "/poke/static/js/2.cfff9107.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/poke/static/js/2.cfff9107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f61281ce353ddb050c0",
+    "url": "/poke/static/js/main.40ad7146.chunk.js"
+  },
+  {
+    "revision": "011bd1177b5368c180bb",
+    "url": "/poke/static/js/runtime-main.051d943c.js"
+  },
+  {
+    "revision": "ae1b16c980de70647c0ab4eb76d7d0a1",
+    "url": "/poke/static/media/face.ae1b16c9.png"
+  },
+  {
+    "revision": "f0269b287a89be5db91fab27c65939ae",
+    "url": "/poke/static/media/oamity.f0269b28.png"
+  }
+]);
